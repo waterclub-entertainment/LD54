f57828d5eb8f36131732f355b5f944e8c5ef896f
Deathlymad <meyerphilip96@googlemail.com>
2023-10-02 15:50:57 +0200

    added sauna

--- Assets/Scripts/NavLogic/NavBuilding.cs
+++ Assets/Scripts/NavLogic/NavBuilding.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using Unity.Mathematics;
-using UnityEditor;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class NavBuilding : MonoBehaviour
