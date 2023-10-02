4f07235960955f1d2769d494ce707bca32f05661
MDeiml <matthias@deiml.net>
2023-10-02 21:25:07 +0200

    Fix idling while doing task

--- Assets/Scripts/RoomIcon.cs
+++ Assets/Scripts/RoomIcon.cs
@@ -16,6 +16,7 @@ public class RoomIcon : MonoBehaviour {
 		GameObject mainCamera = GameObject.FindWithTag("MainCamera");
 		float cameraSize = mainCamera.GetComponent<Camera>().orthographicSize;
 		float opacity = Mathf.Clamp((cameraSize - minCameraSize) / (maxCameraSize - minCameraSize), 0f, 1f);
+		opacity = opacity * 0.7f + 0.3f;
 		var oldColor = spriteRenderer.color;
 		spriteRenderer.color = new Color(oldColor.r, oldColor.g, oldColor.b, opacity);
 	}
